0dbc348a41cda6a4505d0e696b8ebb8d7272f96b
orahbaloyi1 <orahbaloyi1@gmail.com>
2024-02-26 22:57:24 +0200

    Imported react & bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>heloo Orapeleng</h1>
+      <h1 className="bg-warning">heloo Orapeleng Baloyi</h1>
+      <span>ola</span>
+      <p>my name is orapeleng</p>
+      <p>I am A FRONT-END DEVELOPER</p>
     </div>
   );
 }
