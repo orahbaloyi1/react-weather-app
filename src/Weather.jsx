07004bcb71c25c33295df871251fc3d066e31ecf
orahbaloyi1 <orahbaloyi1@gmail.com>
2024-02-27 12:01:49 +0200

    Added css file and structure to the webpage

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -3,20 +3,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather() {
   return (
-    <div className="WeatherApp container">
+    <div className="WeatherApp container mt-5">
       <h2>Joburg</h2>
       <div className="row">
         <div className="col-6">
-          <img
-            href="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="weather image"
-          />
-          <h2>18</h2>
-          <ul>
-            <li>Precipitation: 25%</li>
-            <li>Humidity:92%</li>
-            <li>Wind:10 km/h</li>
-          </ul>
+          <div className="row">
+            <div className="col-4">
+              <img
+                href="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+                alt="weather image"
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-4">
+              <h2>18 C</h2>
+            </div>
+            <div className="col-4">
+              <div className="weather-description">
+                <ul>
+                  <li>Precipitation: 25%</li>
+                  <li>Humidity:92%</li>
+                  <li>Wind:10 km/h</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-6"></div>
       </div>
